ecef36e1ee7bb4ca76d83459a5524450cac4a6df
Timo Müller <timex385@gmail.com>
2026-09-18 13:29:51 +0200

    cfg passthrough

--- cmd/discopanel/main.go
+++ cmd/discopanel/main.go
@@ -73,6 +73,7 @@ func main() {
 		RuntimeImage: cfg.Docker.RuntimeImage,
 		DNS:          cfg.Docker.DNS,
 		Labels:       cfg.Docker.Labels,
+		LogDriver:    cfg.Docker.LogDriver,
 	})
 	if err != nil {
 		log.Fatal("Failed to initialize Docker client: %v", err)
